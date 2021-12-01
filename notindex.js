e7359ce21f9c6980d9f55ffa8f5020c4bb3d7325
Ubuntu <ubuntu@ip-172-31-3-215.eu-west-2.compute.internal>
2021-12-01 16:31:46 +0000

    refactor1

---
+++ notindex.js
@@ -0,0 +1,9 @@
+//const createCluster = require("./eks.js")
+
+//createCluster("eks21")
+
+//createCluster("eks19-dev")
+
+// wait for about 15 minutes untill master endpoint is created
+// Then query existing cluster's clusterSecurityGroupId
+// Then modify this SG to allow access from selected sources.
